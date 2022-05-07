f1e0a98dee21508e9add8c23bf8be0959d7ab422
lydia soltani <lydia.soltani@alumni.univ-avignon.fr>
2022-05-07 20:38:10 +0200

    modif IPokedexFactorytest

--- Projet_MAVEN/mon-appli/src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ Projet_MAVEN/mon-appli/src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,31 +1,4 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-
 public class IPokedexFactoryTest {
-
-    IPokedexFactory pokedexFactory;
-    PokemonMetadata aquali;
-    PokemonMetadata bulbizarre;
-    @Before
-    public void init() {
-
-        pokedexFactory = Mockito.mock(IPokedexFactory.class);
-
-        bulbizarre = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
-        aquali = new PokemonMetadata(133, "Aquali", 186, 186, 260);
-    }
-    @Test
-    public void CreatePokedexTest() {
-        IPokemonMetadataProvider pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
-        IPokemonFactory pokemonFactory = Mockito.mock(IPokemonFactory.class);
-        Mockito.doReturn(Mockito.mock(IPokedex.class)).when(pokedexFactory).createPokedex(Mockito.any(pokemonMetadataProvider.getClass()), Mockito.any(pokemonFactory.getClass()));
-        Assert.assertNotNull(pokedexFactory.createPokedex(Mockito.mock(IPokemonMetadataProvider.class), Mockito.mock(IPokemonFactory.class)));
-        Assert.assertEquals(Mockito.mock(IPokedex.class).getClass(), pokedexFactory.createPokedex(Mockito.mock(IPokemonMetadataProvider.class), Mockito.mock(IPokemonFactory.class)).getClass());
-
-
-    }
 }
