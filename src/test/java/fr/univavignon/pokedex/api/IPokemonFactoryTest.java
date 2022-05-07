d7017158a1d2b959a939ca77558d7c908931bdf4
lydia soltani <lydia.soltani@alumni.univ-avignon.fr>
2022-05-07 20:47:45 +0200

    Tests IPokemonFactoryTest tp3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,4 +1,32 @@
 package fr.univavignon.pokedex.api;
 
-public class IPokemonFactoryTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+public  class IPokemonFactoryTest {  // permet de créer individu
+
+    IPokemonFactory pokemonFactory;
+    Pokemon bulbizarre; Pokemon aquali ; // création des pokémons
+    @Before
+    public void init() {
+
+        pokemonFactory = Mockito.mock(IPokemonFactory.class);
+// initialisation des pokémons
+        bulbizarre =  new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
+        aquali = new Pokemon(133, "Aquali", 186, 186, 260, 2729, 202, 5000, 4, 100.0);
+    }
+
+    @Test
+    public void createPokemonTest() {
+        Mockito.when(pokemonFactory.createPokemon(0, 613, 64, 4000, 4)).thenReturn(bulbizarre);
+        Mockito.when(pokemonFactory.createPokemon(133,2729, 202, 5000, 4)).thenReturn(aquali);
+
+
+    }
+
+
+
+
 }
