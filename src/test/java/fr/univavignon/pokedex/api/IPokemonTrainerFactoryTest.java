ec8738ce0824363eeb6ba3dbaa406dd3f3d56247
lydia soltani <lydia.soltani@alumni.univ-avignon.fr>
2022-05-08 14:19:37 +0200

    Tests IPokemonTrainerFactoryTest tp3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,4 +1,44 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class IPokemonTrainerFactoryTest {
+
+    IPokedex pokedex;
+    IPokemonTrainerFactory trainerFactory; ;
+
+
+    PokemonTrainer trainerPoke;
+    IPokedexFactory factory;
+
+
+    @Before
+    public void init() {
+        pokedex = Mockito.mock(IPokedex.class);
+        trainerFactory = Mockito.mock(IPokemonTrainerFactory.class);
+        factory = Mockito.mock(IPokedexFactory.class);
+
+        trainerPoke = new PokemonTrainer("Lydia", Team.INSTINCT, pokedex);
+
+    }
+
+
+    @Test
+    public void createTrainerTest() {
+
+        Mockito.doReturn(trainerPoke).when(trainerFactory).createTrainer("Lydia", Team.INSTINCT, factory);
+        Assert.assertEquals(trainerPoke.getClass(), trainerFactory.createTrainer("Lydia", Team.INSTINCT, factory).getClass());
+        Assert.assertEquals(trainerPoke, trainerFactory.createTrainer("Lydia", Team.INSTINCT, factory));
+        Assert.assertEquals("Lydia",trainerFactory.createTrainer("Lydia", Team.INSTINCT, factory).getName() );
+        Assert.assertEquals(Team.INSTINCT, trainerFactory.createTrainer("Lydia", Team.INSTINCT, factory).getTeam());
+        Assert.assertEquals(pokedex, trainerFactory.createTrainer("Lydia", Team.INSTINCT, factory).getPokedex());
+        Assert.assertEquals(pokedex.size(), trainerFactory.createTrainer("Lydia", Team.INSTINCT, factory).getPokedex().size());
+
+
+    }
+
+
 }
